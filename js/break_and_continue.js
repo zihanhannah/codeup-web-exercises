9bed85192f299ed402aca80e8cd8ee7a64b19330
zihan <zihanhannah@gmail.com>
2020-06-30 14:23:41 -0500

    break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,21 @@
 "use strict";
-var num = prompt("Please choose a ODD number from 1 to 50");
-function oddNum(num) {
-    while(num % 2 == 0 || num >50){
+var num ;
+
+    while(true){
     num = prompt("Please choose a ODD number from 1 to 50");
-    };
+    num = parseInt(num);
+    if(num % 2 !== 0 && num >=1 && num <50){
+        break;
+    }
+    alert("this is not the valid input");
+    }
+    console.log("number to skip is: " + num);
+    console.log("");
     for (var i = 1; i < 50; i= i+2) {
-        if (i == num) {
+        if (i === num) {
             console.log("Yikes! Skipping number: " + i);
             continue;
         }
         console.log("Here is an odd number: " + i);
     }
-}
-oddNum(num);
+
